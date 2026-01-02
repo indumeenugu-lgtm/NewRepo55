f9f56508d0846e05fecd53c6af324170fef57be9
Indu <Indu@INDULAPTOP-9949>
2026-01-02 17:49:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("wellcome to new words");
+            Console.ReadLine();
+        }
+    }
+}
